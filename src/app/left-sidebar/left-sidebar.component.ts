2d83c14e87b2a7e18693120c7b7aa90cb9655339
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-09-09 01:56:49 +0800

    Update left-sidebar.component.ts

--- src/app/left-sidebar/left-sidebar.component.ts
+++ src/app/left-sidebar/left-sidebar.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'firebase/auth';
+import { CommonModule } from '@angular/common';
+import { AuthDialogComponent } from '../../modals/auth-dialog/auth-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
 export class LeftSidebarComponent { 
   @Input() variant: 'organizer' | 'user' = 'organizer';
+  openAuthDialog() {
+    this.dialog.open(AuthDialogComponent, {
+      width: '400px',
+      panelClass: 'rounded-xl',
+      disableClose: true
+    });
+  }
+
+  user$: Observable<User | null>;
+  constructor(private authService: AuthService, private dialog: MatDialog) {
+    this.user$ = this.authService.authState$;
+    if(this.user$) {
+      console.log(this.user$);
+    }
+  }
+  async logout() {
+    try{
+      await this.authService.logout();
+      console.log('Logged out!');
+    }catch(err: any){
+      console.log(err.message);
+    }
+    
+  }
 }
