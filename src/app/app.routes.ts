bed1a1931b784e9937133802a25af026b5b8a9bd
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-09-08 23:31:43 +0800

    retain pages component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { PagesComponent } from './pages/pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ElectionComponent } from './pages/election/election.component';
 import { ResultComponent } from './pages/result/result.component';
