e31e5fa3070a6954fdd497e177b680f4b4ee5e30
Lord Byron Dizon <dizon.lordbyron@gmail.com>
2025-09-09 15:11:03 +0800

    Update backend.service.ts

--- src/services/backend.service.ts
+++ src/services/backend.service.ts
@@ -20,5 +20,122 @@ export class BackendService {
     return response.data;
   }
 
+  async updateElection(electionId: number, name: string, startDate: string, endDate: string, domainFilter: string) {
+    const response = await axios.post(`${this.apiUrl}/update-election`, {
+      electionId,
+      name,
+      startDate,
+      endDate,
+      domainFilter
+    });
+    return response.data;
+  }
+
+  async closeElection(electionId: number) {
+    const response = await axios.post(`${this.apiUrl}/close-election`, {
+      electionId
+    });
+    return response.data;
+  }
+
+
+  async getOwnedElections(email: string) {
+    const response = await axios.post(`${this.apiUrl}/get-owned-elections`, {
+      email
+    });
+    return response.data;
+  }
+
   
+  async getOwnedElectionNames(email: string) {
+    const response = await axios.post(`${this.apiUrl}/get-owned-election-names`, {
+      email
+    });
+    return response.data;
+  }
+
+  
+  async getOwnedElectionIds(email: string) {
+    const response = await axios.post(`${this.apiUrl}/get-owned-election-ids`, {
+      email
+    });
+    return response.data;
+  }
+
+  
+  async getElectionIdByName(name: string) {
+    const response = await axios.post(`${this.apiUrl}/get-election-id-by-name`, {
+      name
+    });
+    return response.data;
+  }
+
+  
+
+  async getElectionResults(electionId: number) {
+    const response = await axios.post(`${this.apiUrl}/get-election-results`, {
+      electionId
+    });
+    return response.data;
+  }
+
+  
+
+  async getElectionDetails(electionId: number) {
+    const response = await axios.post(`${this.apiUrl}/get-election-details`, {
+      electionId
+    });
+    return response.data;
+  }
+
+  
+
+  async getElectionCandidateCount(electionId: number) {
+    const response = await axios.post(`${this.apiUrl}/get-election-candidate-count`, {
+      electionId
+    });
+    return response.data;
+  }
+
+  
+  async addCandidates(electionId: number, candidates: {name: string, position: string, platform: string, cdn: string}[]) {
+    const response = await axios.post(`${this.apiUrl}/add-candidates`, {
+      electionId,
+      candidates
+    });
+    return response.data;
+  }
+
+  async updateCandidate(electionId: number, candidateId: number, name: string, position: string, platform: string, cdn: string) {
+    const response = await axios.post(`${this.apiUrl}/update-candidate`, {
+      electionId,
+      candidateId,
+      name,
+      position,
+      platform,
+      cdn
+    });
+    return response.data;
+  }
+
+  async deleteCandidate(electionId: number, candidateId: number) {
+    const response = await axios.post(`${this.apiUrl}/delete-candidate`, {
+      electionId,
+      candidateId
+    });
+    return response.data;
+  }
+
+
+  async getElectionCandidate(electionId: number, candidateId: number) {
+    const response = await axios.post(`${this.apiUrl}/get-election-candidate`, {
+      electionId,
+      candidateId
+    });
+    return response.data;
+  }
+
+  
+
+
 }
