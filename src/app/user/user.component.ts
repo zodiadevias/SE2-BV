721485df4dba12652062c9eb81a94282841656f3
Lance-Ceo <annasabanal02@gmail.com>
2025-10-05 01:06:59 +0800

    bug fixes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeftSidebarComponent } from '../left-sidebar/left-sidebar.component';
 
@@ -9,6 +9,34 @@ import { LeftSidebarComponent } from '../left-sidebar/left-sidebar.component';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   currentYear = new Date().getFullYear();
+  isMobile = false;
+  mobileMenuOpen = false;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+    if (!this.isMobile) {
+      this.mobileMenuOpen = false;
+    }
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    if (this.isMobile) {
+      this.mobileMenuOpen = false;
+    }
+  }
 }
